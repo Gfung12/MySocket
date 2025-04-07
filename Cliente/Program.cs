76c5e11689f71cb1e13e1077206229415dd7d328
Adam <gokfungchengliang@gmail.com>
2025-04-07 11:03:59 -0600

    Programs

---
+++ Cliente/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySockectLibrary;
+using System.Net;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new SocketClient();
+
+        try
+        {
+            Console.WriteLine("Conectando al servidor...");
+            await client.ConnectAsync("127.0.0.1", 8080);
+
+            while (true)
+            {
+                Console.Write("Escribe un mensaje (o 'salir' para terminar): ");
+                var message = Console.ReadLine();
+
+                if (message?.ToLower() == "salir")
+                    break;
+
+                await client.SendAsync(message!);
+            }
+        }
+        finally
+        {
+            client.Disconnect();
+            Console.WriteLine("Desconectado");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using MySockectLibrary;
+using System.Net;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var server = new SocketServer(8080); // Puerto 8080
+        Console.WriteLine("Iniciando servidor... (Presiona Ctrl+C para detener)");
+
+        var serverTask = server.StartAsync();
+
+        // Mantener el servidor corriendo hasta que se presione una tecla
+        Console.ReadKey();
+        server.Stop();
+        Console.WriteLine("Servidor detenido");
+    }
+}
